691cef03f3624159833b102b8bbbc1ace04c3d63
indermohansingh <indermohansingh@hotmail.com>
2020-05-02 17:24:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Hey tHere, Buddy!';
+  const title = 'Hey there, Buddy!';
   const options = {
     body: `${event.data.text()}`,
     icon: 'images/icon.png',
