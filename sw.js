fce51e52d32e7fa8408b10a668a227eeeaab5f14
indermohansingh <indermohansingh@hotmail.com>
2020-05-02 19:13:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,7 +29,8 @@ self.addEventListener('push', function(event) {
   const options = {
     body: `${event.data.text()}`,
     icon: 'images/icon.png',
-    badge: 'images/badge.png'
+    badge: 'images/badge.png',
+    requireInteraction: true
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
